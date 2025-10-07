497cc7873d13f752f629b19440ebf2b9f3b68008
soarbh srivastava <soarbh1234@gmail.com>
2025-10-07 10:27:20 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/components/layout/app-sidebar.tsx
+++ src/components/layout/app-sidebar.tsx
@@ -43,19 +43,20 @@ export default function AppSidebar() {
         <SidebarMenu>
           {navItems.map((item) => (
             <SidebarMenuItem key={item.href}>
-              <Link href={item.href} passHref legacyBehavior>
-                <SidebarMenuButton
-                  isActive={
-                    item.href === '/'
-                      ? pathname === item.href
-                      : pathname.startsWith(item.href)
-                  }
-                  tooltip={item.label}
-                >
+              <SidebarMenuButton
+                asChild
+                isActive={
+                  item.href === '/'
+                    ? pathname === item.href
+                    : pathname.startsWith(item.href)
+                }
+                tooltip={item.label}
+              >
+                <Link href={item.href}>
                   {item.icon}
                   <span>{item.label}</span>
-                </SidebarMenuButton>
-              </Link>
+                </Link>
+              </SidebarMenuButton>
             </SidebarMenuItem>
           ))}
         </SidebarMenu>
